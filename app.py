88643687050b8c7dd8cbff28939040d8de9a9871
JuanSO121 <sanchezjuanjose0508@gmail.com>
2024-05-23 21:24:49 -0500

    home

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 from blueprints.index.index import index_bp
 from extensions import configure_extensions
 
 app = Flask(__name__)
 
-#Seguridad de información
+# Seguridad de información
 app.config['SECRET_KEY'] = 'your_secret_key'
-#Carpetas Imagenes
+# Carpetas Imagenes
 app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
-#Imagenes Confi
+# Imagenes Confi
 configure_extensions(app)
-#Blueprints modularizar codigo
+# Blueprints modularizar codigo
 app.register_blueprint(index_bp)
+
+# Ruta para renderizar el home.html
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ =='__main__':   
     app.run(debug=True)
